a4942fbcbf415999beb36373d2ac47e92d376c9c
Jarkko Sakkinen <jarkko.sakkinen@iki.fi>
2019-01-18 15:22:00 +0200

    Allocate SurgeVoice with the new-operator
    By using alignas() and adding an empty default constructor, we can
    preallocate SurgeVoice instances in the same way we would allocate them
    with _aligned_malloc().
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@iki.fi>

--- src/common/dsp/SurgeVoice.cpp
+++ src/common/dsp/SurgeVoice.cpp
@@ -37,6 +37,10 @@ float SurgeVoiceState::getPitch()
           detune;
 }
 
+SurgeVoice::SurgeVoice()
+{
+}
+
 SurgeVoice::SurgeVoice(SurgeStorage* storage,
                        SurgeSceneStorage* oscene,
                        pdata* params,
